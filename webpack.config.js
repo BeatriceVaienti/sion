d40ea7d938cd7c25abf1c206d8bd41a08256f427
BeatriceVaienti <beatrice.vaienti@yahoo.it>
2022-03-16 10:32:24 +0100

    changed src folder name

--- webpack.config.js
+++ webpack.config.js
@@ -4,7 +4,7 @@
  const HtmlWebpackPlugin = require('html-webpack-plugin');
 
  const dist = path.resolve(__dirname, 'dist');
- const src = path.resolve(__dirname, 'src');
+ const src = path.resolve(__dirname, 'docs');
 
  module.exports = {
    entry: path.resolve(src, 'index.js'),
