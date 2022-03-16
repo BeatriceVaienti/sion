efc1f81f51b518b2a01193e1182e44c5d8136d92
BeatriceVaienti <beatrice.vaienti@yahoo.it>
2022-03-16 10:42:04 +0100

    going back to previous folder name

--- webpack.config.js
+++ webpack.config.js
@@ -4,7 +4,7 @@
  const HtmlWebpackPlugin = require('html-webpack-plugin');
 
  const dist = path.resolve(__dirname, 'dist');
- const src = path.resolve(__dirname, 'docs');
+ const src = path.resolve(__dirname, 'src');
 
  module.exports = {
    entry: path.resolve(src, 'index.js'),
