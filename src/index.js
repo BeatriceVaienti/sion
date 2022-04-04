2b727008ba8a95de2eb4d140ec36ae5a9c6f9516
BeatriceVaienti <beatrice.vaienti@yahoo.it>
2022-04-04 17:19:06 +0200

    new functionalities

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-import { Scene, ShaderLib, RGBAFormat, NoBlending, PointLightHelper, NearestFilter, WebGLRenderTarget,  CameraHelper, ShaderMaterial, Raycaster, UniformsUtils, DirectionalLightHelper, Vector2, Object3D, WebGLRenderer, UnsignedByteType, PMREMGenerator,SphereGeometry, Mesh, MeshStandardMaterial, DirectionalLight, PCFSoftShadowMap, AmbientLight, PerspectiveCamera, Fog, Vector3, Box3, PointLight} from "three";
+import { Scene, ShaderLib, RGBAFormat, NoBlending, PointLightHelper, NearestFilter, WebGLRenderTarget,  CameraHelper, ShaderMaterial, Raycaster, UniformsUtils, DirectionalLightHelper, Vector2, Object3D, WebGLRenderer, UnsignedByteType, PMREMGenerator,SphereGeometry, Mesh, MeshStandardMaterial, MeshPhysicalMaterial, DirectionalLight, PCFSoftShadowMap, AmbientLight, PerspectiveCamera, Fog, Vector3, Box3, PointLight} from "three";
 import { GLTFLoader } from "three/examples/jsm/loaders/GLTFLoader";
 import { OrbitControls } from "three/examples/jsm/controls/OrbitControls";
 import { EffectComposer } from 'three/examples/jsm/postprocessing/EffectComposer.js';
@@ -8,10 +8,13 @@ import { RGBELoader } from 'three/examples/jsm/loaders/RGBELoader';
 import { OutlinePass } from 'three/examples/jsm/postprocessing/OutlinePass.js';
 import { ShaderPass } from 'three/examples/jsm/postprocessing/ShaderPass.js';
 import { SSAOShader } from 'three/examples/jsm/shaders/SSAOShader.js';
-import { HDRCubeTextureLoader } from 'three/examples/jsm/loaders/HDRCubeTextureLoader'
-
+import { HDRCubeTextureLoader } from 'three/examples/jsm/loaders/HDRCubeTextureLoader';
+import { SAOPass } from 'three/examples/jsm/postprocessing/SAOPass.js';
 // Import our glTF model.
 import gltfUrl from "../scene/sion_deployment.gltf";
+
+
+
 
 // Create the renderer and scene
 const scene = new Scene();
@@ -37,26 +40,13 @@ controls.target.set(1000,0, -1200);
 controls.zoomSpeed = 1
 controls.update();
 
-
-// Load envMap. not working
-// var pmremGenerator = new PMREMGenerator( renderer );
-// pmremGenerator.compileEquirectangularShader();
-// var rgbeLoader = new RGBELoader()
-//         .setDataType( UnsignedByteType )
-//         .setPath( 'hdr/' );
-// var texture = rgbeLoader.load('venice_sunset_1k.hdr');
-// var envMap = pmremGenerator.fromEquirectangular(texture).texture;
-// scene.background = envMap;
-// scene.environment = envMap;
-
-
-const amblight = new AmbientLight(0x404040, 0.7);
+const amblight = new AmbientLight(0x404040, 0.2);
 scene.add(amblight);
 
-scene.fog = new Fog('black', 0, 2000);
+scene.fog = new Fog('black', 0, 1500);
 
 //Create a DirectionalLight and turn on shadows for the light
-const light = new DirectionalLight( 0xffffff, 1);
+const light = new DirectionalLight( 0xffffff,  0.8);
 light.position.set( 1000, 2000, 500); //default; light shining from top
 light.castShadow = true; // default false
 scene.add( light );
@@ -74,13 +64,13 @@ light.shadow.camera.bottom = - d;
 
 
 
-const light2 = new PointLight( 0xfff8dc,0.4);
+const light2 = new PointLight( 0xfff8dc,0.2);
 light2.position.set(800, 300,-1300); //default; light shining from top
 light2.castShadow = false; // default false
 scene.add( light2 );
 
 
-const light3 = new PointLight(0xb0e0e6	,0.4);
+const light3 = new PointLight(0xb0e0e6	,0.2);
 light3.position.set(900, 150,-1150); //default; light shining from top
 scene.add(light3);
 light3.castShadow = false;
@@ -94,10 +84,12 @@ light3.decay = 3;
 // scene.add(helper3);
 
 
-const wall = ()=>new MeshStandardMaterial({color: 0xffffff, metalness: 0, roughness:1});
-const roof = ()=>new MeshStandardMaterial({color: 0x808080, metalness: 0, roughness: 1});
+const wall = ()=>new MeshStandardMaterial({color: 0xdcdcdc, metalness: 0, roughness:1});
+const roof = ()=>new MeshStandardMaterial({color: 0x808080, metalness: 0.5, roughness: 1});
 const ground = ()=>new MeshStandardMaterial({color: 0x776e69, metalness: 0, roughness: 1});
-
+wall.envMapIntensity = 0.1 ;
+roof.envMapIntensity = 0.1
+console.log(wall)
  // Load the glTF model and add it to the scene.
  const loader = new GLTFLoader();
  loader.load(gltfUrl, (gltf) => {   
@@ -127,10 +119,37 @@ const ground = ()=>new MeshStandardMaterial({color: 0x776e69, metalness: 0, roug
     
   });
  });
+
+
+
+// Load envMap
+// don't know why but the locally saved file was not found, I needed to use a url
+const pmremGenerator = new PMREMGenerator(renderer);
+pmremGenerator.compileEquirectangularShader();
+const rgbeLoader = new RGBELoader();
+
+rgbeLoader.load('https://threejs.org/examples/textures/equirectangular/venice_sunset_1k.hdr', function(texture) {
+
+  const envMap = pmremGenerator.fromEquirectangular(texture).texture;
+
+  //scene.background = envMap;
+  scene.environment = envMap;
+
+
+  texture.dispose();
+  pmremGenerator.dispose();
+
+  });
+
+
+
 const composer = new EffectComposer(renderer);
+let renderPass = new RenderPass(scene, camera);
+composer.addPass( renderPass );
+const saoPass = new SAOPass( scene, camera, false, true);
+saoPass.setSize(window.innerWidth, window.innerHeight)
 
-
-var renderPass = new RenderPass(scene, camera);
+console.log(composer)
 // composer.addPass(renderPass);
 // const outlinePass = new OutlinePass( new Vector2(window.innerWidth, window.innerHeight), scene, camera);
 // outlinePass.edgeStrength = Number( 1 );
@@ -139,7 +158,29 @@ var renderPass = new RenderPass(scene, camera);
 // // outlinePass.pulsePeriod = Number( 0 );
 // outlinePass.visibleEdgeColor.set( "#ffffff" );
 // outlinePass.hiddenEdgeColor.set( "#000000" ); 
-// composer.addPass(outlinePass);
+// com
+
+// // depth
+// var depthShader = ShaderLib[ "distanceRGBA" ]; // Depth encoding into RGBA texture
+// var depthUniforms = UniformsUtils.clone( depthShader.uniforms );
+// const depthMaterial = new ShaderMaterial( { fragmentShader: depthShader.fragmentShader,
+// vertexShader: depthShader.vertexShader, uniforms: depthUniforms } );
+// depthMaterial.blending = NoBlending;
+// const depthTarget = new WebGLRenderTarget( window.innerWidth, window.innerHeight,
+// { minFilter: NearestFilter, magFilter: NearestFilter, format: RGBAFormat } );
+
+// // postprocessing
+// //const composer = new EffectComposer( renderer );
+// composer.addPass( new RenderPass( scene, camera ) );
+// var effect = new ShaderPass( SSAOShader );
+// console.log(effect)
+// effect.uniforms[ 'tDepth' ].value = depthTarget;
+// effect.uniforms[ 'resolution' ].value.set( window.innerWidth, window.innerHeight );
+// effect.uniforms[ 'cameraNear' ].value = camera.near;
+// effect.uniforms[ 'cameraFar' ].value = camera.far;
+// effect.renderToScreen = true;
+// composer.addPass( effect );
+
 
 // Instruct the engine to resize when the window does.
 window.addEventListener('resize', onWindowResize, false);
@@ -170,8 +211,15 @@ function fill_selected_object_owner_info(userData){
   const owners= from_userData_to_owner_info_list(userData)
   //console.log("owners", owners)
   //<tr> <td>1648 - 1656</td><td>prob. Carloz, Carlo (y c. G 51)</td></tr>
-  document.getElementById("owner-table").innerHTML=  owners.map(o=> "<tr> <td>"+o.start_year+" - "+o.end_year+"</td><td>"+o.name+"</td></tr>").join("\n")
+  console.log(owners.length)
+  if(owners.length===0){
+    document.getElementById("owner-table").innerHTML=  ("<tr> <td>"+"-"+"</td><td>"+"-"+"</td></tr>")
+  } else {
+    document.getElementById("owner-table").innerHTML=  owners.map(o=> "<tr> <td>"+o.start_year+" - "+o.end_year+"</td><td>"+o.name+"</td></tr>").join("\n");
+  }
 }
+
+
 
 function onPointerMove( event ) {
 
@@ -188,15 +236,16 @@ function onPointerMove( event ) {
 
 var old_selected_building = null
 function onClick(){
-
+  document.getElementById("owner-info").style.visibility = "hidden";
   const selected_building = get_closest_intersected_building()
 
   if(selected_building!==null){
+    document.getElementById("owner-info").style.visibility = "visible";
     selected_building.object.parent.children.forEach(c=>c.material.emissive.set( "blue" ))
     //console.log(selected_building.object.parent.parent.userData)
     //console.log(selected_building.object.material.emissive)
     fill_selected_object_owner_info(selected_building.object.parent.parent.userData)
-
+    
     if(old_selected_building!==null &&  old_selected_building.object.id!=selected_building.object.id){
       old_selected_building.object.parent.children.forEach(c=>c.material.emissive.set( "black" ))
     }
@@ -211,10 +260,10 @@ function get_closest_intersected_building(){
   // calculate objects intersecting the picking ray
   const intersects = raycaster.intersectObjects( scene.children );
   
-  if(intersects.length>0 && intersects[0].object.name != "0_LoD1_tin"){
-    return intersects[0]
+  if(intersects.length>0 && intersects[0].object.parent.parent.userData.type  === "Building"){
+    return intersects[0];
   }
-  return null
+  return null;
 }
 
 var old_intersected_building = null
@@ -241,26 +290,6 @@ function render() {
 canvas.addEventListener( 'pointermove', onPointerMove );
 canvas.addEventListener( 'click', onClick );
 
-// depth
-var depthShader = ShaderLib[ "distanceRGBA" ]; // Depth encoding into RGBA texture
-var depthUniforms = UniformsUtils.clone( depthShader.uniforms );
-const depthMaterial = new ShaderMaterial( { fragmentShader: depthShader.fragmentShader,
-vertexShader: depthShader.vertexShader, uniforms: depthUniforms } );
-depthMaterial.blending = NoBlending;
-const depthTarget = new WebGLRenderTarget( window.innerWidth, window.innerHeight,
-{ minFilter: NearestFilter, magFilter: NearestFilter, format: RGBAFormat } );
-
-// postprocessing
-//const composer = new EffectComposer( renderer );
-composer.addPass( new RenderPass( scene, camera ) );
-var effect = new ShaderPass( SSAOShader );
-console.log(effect)
-effect.uniforms[ 'tDepth' ].value = depthTarget;
-//effect.uniforms[ 'resolution' ].value.set( window.innerWidth, window.innerHeight );
-//effect.uniforms[ 'cameraNear' ].value = camera.near;
-//effect.uniforms[ 'cameraFar' ].value = camera.far;
-effect.renderToScreen = true;
-composer.addPass( effect );
 
 
 // Start the engine's main render loop.
